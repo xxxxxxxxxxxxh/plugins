413daccd11d8241df039627e2cf071dbd376e947
xhx <594429499@qq.com>
2019-09-14 11:33:46 +0800

    card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <!--<img alt="Vue logo" src="../assets/logo.png">-->
+		<!--<p>{{messege}}</p>-->
     <calendar
 			:walk="false"
 			:clickEvent="true" 
@@ -16,13 +17,24 @@
 		<!--<datepicker :defaultType="type" :default="b" :defaultList="defaultList" ></datepicker>-->
 		<!--<datepicker :defaultType="false" :default="a"></datepicker>-->
     <card :isShadow='true' :isHorizontal="false">
-			<h2>默认slot</h2>
+			<h2>Card卡片</h2>
 			<template v-slot:customize>
 				<div style="color: #f00;">
 					<p>slot插槽</p>
+					<p>插槽名customize</p>
+					<p>插槽颜色红色</p>
 				</div>
 			</template>
 		</card>
+		<input-text
+			style="margin-top: 15px; margin-left: 5vw;"
+			:isCurve="true" 
+			:placeholder="placeholder"
+			:value="test"
+			></input-text>
+		<div @click="m">
+			{{test}}
+		</div>
   </div>
 </template>
 
@@ -32,19 +44,22 @@
 import Calendar from "../components/calendar/calendar";
 import Datepicker from "../components/datepicker/datepicker";
 import Card from "../components/card/card";
+import InputText from "../components/input/input";
 
 export default {
-  components: {Calendar, Datepicker, Card},
+  components: {Calendar, Datepicker, Card, InputText},
   name: 'home',
 	data () {
 		return {
 			eventData: [
 				
 			],
+			test: 'v-model双向绑定',
+			messege: null,
 			type: true,
-			false: false,
 			a: '2019-8-15',
 			b: '宁波',
+			placeholder: '请输入搜索内容',
 			defaultList: [{column: ['杭州', '宁波', '温州', '嘉兴', '湖州', '丽水']}],
 		}
 	},
@@ -52,14 +67,21 @@ export default {
 		getData: function (res) {
 			console.log(res)
 		},
+		m: function() {
+			alert(1)
+		}
     
 	},
-	async created () {
-		// let res = await fetch('http://127.0.0.1:3030/exam');
-		// let data = await res.json();
-		// console.log(data)
-		// this.eventData = data.eventData
-		//this.defaultList = data.defaultList
+	/**async created () {
+		let res = await fetch('http://127.0.0.1:3030/exam');
+		let data = await res.json();
+		console.log(data)
+		this.messege = data
+	}**/
+	created () {
+		fetch('http://127.0.0.1:3030/exam').then(res => res.json()).then(data => {
+			this.messege = data
+		})
 	}
 }
 </script>
