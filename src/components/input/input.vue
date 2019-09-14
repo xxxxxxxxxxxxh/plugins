b8b7489629f50884d15b17bd8ad83da043fa15cb
xhx <594429499@qq.com>
2019-09-14 11:58:51 +0800

    9.14

---
+++ src/components/input/input.vue
@@ -0,0 +1,97 @@
+<template>
+	<div class="inputText" :class="{curve: isCurve}">
+		<input 
+			ref="input" 
+			type="text"
+			:placeholder="placeholder"
+			v-model="inputValue"
+			@keyup.13="submit"
+		>
+		<div 
+			v-if="show"
+			class="close" 
+			@click="clear"
+		>×</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'inputText',
+		data () {
+			return {
+				show: false,
+			}
+		},
+		model: {
+			prop: 'value',
+			event: 'input'
+		},
+		props: {
+			isCurve: Boolean,
+			placeholder: String,
+			value: String
+		},
+		computed: {
+			inputValue: {
+				get: function() {
+					return this.value;
+				},
+				set: function(value) {
+					this.$emit('input', value)
+				}
+			}
+		},
+		methods: {
+			change: function(e) {
+				// console.log(e)
+				console.log(this.value)
+				if (!this.value) {
+					this.show = false;
+				}
+			},
+			submit: function() {
+				alert('submit')
+			},
+			clear: function() {
+				this.value = '';
+				this.show = false;
+			}
+		},
+		mounted () {
+			this.show = this.value.length ? true : false
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+  .inputText {
+		width: 80%;
+		height: 38px;
+		border: 1px solid #c7c7c7;
+		display: flex;
+		input[type="text"] {
+			width: 80%;
+			height: 36px;
+			padding-left: 8px;
+			font-size: 16px;
+			border: none;
+			outline: none;
+		}
+		.close {
+			width: 14px;
+			height: 14px;
+			line-height: 14px;
+			margin-top: 10px;
+			margin-left: 10%;
+			font-size: 20px;
+			background: #999;
+			color: #fff;
+			border-radius: 50%;
+		}
+	}
+	.curve {
+		border-radius: 30px;
+		padding-left: 15px;
+	}
+</style>
